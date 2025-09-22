b36c9638434315b1d6fb94873ad6609dfa1ef8f6
pjamjumrus-DGM <pjamjumrus@gmail.com>
2025-09-22 15:13:49 -0700

    This is  lab1 assignment : temperature converter

---
+++ temperature.js
@@ -0,0 +1,8 @@
+let fahrenheit = 86 // Bangkok, Thailand - November average temperature, 2024
+
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log("Temperature (fahrenheit): " + fahrenheit)
+console.log("Temperature (celsius): " + celsius)
+console.log("Temperature (kelvin): " + kelvin)
